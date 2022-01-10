8939fcee5e89156ec0a065673693befae705bfd4
Bluewind <bluewind@kakao.com>
2022-01-10 11:27:44 +0900

    Add viewing board detail function

--- src/main/java/com/cos/blog/controller/BoardController.java
+++ src/main/java/com/cos/blog/controller/BoardController.java
@@ -9,6 +9,7 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class BoardController {
@@ -26,5 +27,11 @@ public class BoardController {
     @GetMapping("/board/save_form")
     public String saveForm() {
         return "board/save_form";
+    }
+
+    @GetMapping("/board/{id}")
+    public String findById(@PathVariable Long id, Model model) {
+        model.addAttribute("board", boardService.viewBoardDetail(id));
+        return "board/detail";
     }
 }

5ccbf6124072a8ad28442fad05187cbb5f3b5cdc
Bluewind <bluewind@kakao.com>
2022-05-21 11:52:44 +0900

    정상화

--- src/main/java/com/cos/blog/BlogApplication.java
+++ src/main/java/com/cos/blog/BlogApplication.java
@@ -2,10 +2,13 @@ package com.cos.blog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class BlogApplication {
+public class BlogApplication extends SpringBootServletInitializer {
+
 	public static void main(String[] args) {
 		SpringApplication.run(BlogApplication.class, args);
 	}
+
 }
